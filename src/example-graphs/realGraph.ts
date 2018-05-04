6a25a945a0e22f7c86af090e2c3eefc6f4eaede2
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-05-04 15:56:27 +0200

    adding legacy graph converter

--- src/example-graphs/realGraph.ts
+++ src/example-graphs/realGraph.ts
@@ -1,35 +1,47 @@
 import * as legacyGraph from './realGraphLegacy'
 import * as model from '../model'
 
-export const systemNode: model.Node = {
-  id: legacyGraph.graph.name
-}
+export const systemNode: model.Node = convertLegacySystemToNode(legacyGraph.graph)
 
-addServicesAsNodes(legacyGraph.graph, systemNode)
+function convertLegacySystemToNode(system: legacyGraph.System): model.Node {
+  let node: model.Node = {
+    id: system.name,
+    label: system.name
+  }
 
-function addServicesAsNodes(inGraph: legacyGraph.System, outNode: model.Node) {
-  outNode.nodes = []
-  inGraph.services.forEach((service) => {
-    let node: model.Node = {
-      id: service.name,
-      label: service.name
-    }
-    if (service.name.startsWith('exchange')) {
-      node.kind = 'exchange'
-      node.label = service.name.substring('exchange '.length)
-    }
-    outNode.nodes.push(node)
+  node.nodes = []
+  system.services.forEach((service) => {
+    node.nodes.push(convertServiceToNode(service))
 
-    inGraph.subSystems.forEach((subSystem) => {
-      let subSystemNode: model.Node = {
-        id: subSystem.name,
-        label: subSystem.name
-      }
-      outNode.nodes.push(subSystemNode)
-
-      addServicesAsNodes(subSystem, subSystemNode)
+    system.subSystems.forEach((subSystem) => {
+      node.nodes.push(convertLegacySystemToNode(subSystem))
     })
   })
+
+  node.edges = []
+  system.links.forEach((link) => {
+    node.edges.push(convertLinkToEdge(link))
+  })
+
+  return node
 }
 
-console.log(JSON.stringify(systemNode, null, 2))
+function convertServiceToNode(service: legacyGraph.Service): model.Node {
+  let node: model.Node = {
+    id: service.name,
+    label: service.name
+  }
+  if (service.name.startsWith('exchange')) {
+    node.kind = 'exchange'
+    node.label = service.name.substring('exchange '.length)
+  }
+  return node
+}
+
+function convertLinkToEdge(link: legacyGraph.Link): model.Edge {
+  return {
+    sourceNode: link.sourceName,
+    targetNode: link.targetName,
+    kind: link.communicationType
+  }
+}
