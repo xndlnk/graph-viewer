05d0cdf2b6c620b832857df7edff1d8fe5e4fb10
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-05-04 17:03:28 +0200

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,29 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import { SimpleGraph } from './SimpleGraph'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/simplegraph">Simple Graph</Link>
+            </li>
+          </ul>
+          <hr />
+
+          <Route path="/simplegraph" component={SimpleGraph} />
+        </div>
+      </Router>
+    )
+  }
+}
 
 const container = document.getElementById('root')
-const renderApp = () => ReactDOM.render(<SimpleGraph />, container)
+const renderApp = () => ReactDOM.render(<App />, container)
 
 renderApp()
 
