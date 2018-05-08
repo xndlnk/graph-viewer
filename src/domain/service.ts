0e4706fac8967db08c67069d13616539c0f0fb48
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-05-08 07:55:45 +0200

    refactoring

--- src/domain/service.ts
+++ src/domain/service.ts
@@ -2,18 +2,21 @@ import { Node, Edge } from './model'
 import * as _ from 'lodash'
 
 export class GraphService {
-  graph: Node
+  private graph: Node
+  private allNodes: Node[]
+  private allEdges: Edge[]
 
+  /** the nodes and edges of the provided graph must not change while using the GraphService. */
   constructor(graph: Node) {
     this.graph = graph
   }
 
   findNode(nodeId: string): Node {
-    return this.getAllNodes(this.graph).find(node => node.id === nodeId)
+    return this.getAllNodes().find(node => node.id === nodeId)
   }
 
   getNeighbourNodes(node: Node): Node[] {
-    let allEdges = this.getAllEdges(this.graph)
+    let allEdges = this.getAllEdges()
     let sourceNodeIds = allEdges
       .filter((edge) => edge.targetNode === node.id)
       .map((edge) => edge.sourceNode)
@@ -22,28 +25,42 @@ export class GraphService {
       .filter((edge) => edge.sourceNode === node.id)
       .map((edge) => edge.targetNode)
 
-    let neighbourNodes = this.getAllNodes(this.graph)
+    let neighbourNodes = this.getAllNodes()
       .filter((node) => sourceNodeIds.includes(node.id) || targetNodeIds.includes(node.id))
 
     return neighbourNodes
   }
 
   getNeighbourEdges(nodeId: string): Edge[] {
-    return this.getAllEdges(this.graph)
+    return this.getAllEdges()
       .filter((edge) => edge.sourceNode === nodeId || edge.targetNode === nodeId)
   }
 
-  getAllEdges(root: Node): Edge[] {
+  getAllEdges(): Edge[] {
+    if (!this.allEdges) {
+      this.allEdges = this.computeAllEdges(this.graph)
+    }
+    return this.allEdges
+  }
+
+  getAllNodes(): Node[] {
+    if (!this.allNodes) {
+      this.allNodes = this.computeAllNodes(this.graph)
+    }
+    return this.allNodes
+  }
+
+  private computeAllEdges(root: Node): Edge[] {
     if (root.edges) {
-      return _.union(root.edges, _.flatten(root.nodes.map(node => this.getAllEdges(node))))
+      return _.union(root.edges, _.flatten(root.nodes.map(node => this.computeAllEdges(node))))
     } else {
       return []
     }
   }
 
-  getAllNodes(root: Node): Node[] {
+  private computeAllNodes(root: Node): Node[] {
     if (root.nodes) {
-      return _.union(root.nodes, _.flatten(root.nodes.map(node => this.getAllNodes(node))))
+      return _.union(root.nodes, _.flatten(root.nodes.map(node => this.computeAllNodes(node))))
     } else {
       return []
     }
