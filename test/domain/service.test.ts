c22283a9183c72b7182b5635576d428ab1543251
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-05-09 18:34:41 +0200

    added first real test for graph service

---
+++ test/domain/service.test.ts
@@ -0,0 +1,25 @@
+import { Node } from '../../src/domain/model'
+import { GraphService } from '../../src/domain/service'
+
+test('find nodes by id', () => {
+  let graph: Node = {
+    id: 'test-graph',
+    nodes: [
+      {
+        id: 'a'
+      },
+      {
+        id: 'b',
+        nodes: [
+          {
+            id: 'c'
+          }
+        ]
+      }
+    ]
+  }
+  let graphService = new GraphService(graph)
+
+  expect(graphService.findNode('a')).toEqual({ id: 'a' })
+  expect(graphService.findNode('c')).toEqual({ id: 'c' })
+})
